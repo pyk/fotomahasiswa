66a49843b237b7293ee198308b7cd9e5cf202362
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2014-01-06 16:08:58 +0700

    add display: faculty and year

--- app.rb
+++ app.rb
@@ -3,6 +3,22 @@ require "bundler/setup"
 
 require "sinatra"
 
+FAKULTAS = {
+    "01" => "Fakultas Kedokteran",
+    "02" => "Fakultas Kedokteran Gigi",
+    "03" => "Fakultas Hukum",
+    "04" => "Fakultas Ekonomi Dan Bisnis",
+    "05" => "Fakultas Farmasi",
+    "06" => "Fakultas Kedokteran Hewan",
+    "07" => "Fakultas Ilmu Sosial dan Ilmu Politik",
+    "08" => "Fakultas Sains Dan Teknologi",
+    "10" => "Fakultas Kesehatan Masyarakat",
+    "11" => "Fakultas Psikologi",
+    "12" => "Fakultas Ilmu Budaya",
+    "13" => "Fakultas Keperawatan",
+    "14" => "Fakultas Perikanan dan Kelautan"
+  }
+
 get "/" do
   erb :index
 end
@@ -13,5 +29,8 @@ end
 
 get "/:nim" do |nim|
   @nim = nim
+  kode_fakultas = nim.to_s[0,2]
+  @nama_fakultas = FAKULTAS[kode_fakultas]
+  @angkatan = nim.to_s[2,2]
   erb :fotomahasiswa
 end
